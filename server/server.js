3f6aea6118cd7a1f8f2ed7279d1ffbe371cf85d4
Jacob <jeck99@gmail.com>
2021-08-09 16:24:00 +0300

    herko changes

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const cors = require("cors");
 const studentRouter = require('./routes/studentsRouter');
 const db = require('./DB');
+const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,7 +16,15 @@ db.on('error',()=>{console.log("connection error")});
 app.listen(PORT, () => {
     console.log(`server is up on port: ${PORT}`);
 })
-app.get('/', (req, res) => {
-    res.send("success");
-})
-app.use('/students', studentRouter);
+
+app.use('/api/students', studentRouter);
+//*****************************************************************/
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+  }
+//*******************************************************************/
